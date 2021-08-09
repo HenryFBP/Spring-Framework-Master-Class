28c9cfc93cad001205cbbec5aecb6daeb85f781e
Henry Post <henryfbp@gmail.com>
2021-08-09 14:53:47 -0500

    add lifecycle annotations

--- spring-in-5-steps-level-2/src/main/java/com/henry/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ spring-in-5-steps-level-2/src/main/java/com/henry/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,9 +1,14 @@
 package com.henry.spring.basics.springin5steps.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
@@ -11,11 +16,23 @@ import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROT
 @Scope(SCOPE_PROTOTYPE) //give a new bean whenever requested...
 public class BinarySearchImpl {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     @Qualifier("quick")
     private SortAlgorithm sortAlgorithm;
 
     // private SortAlgorithm bubbleSortAlgorithm; //require dep by name. Spring actually looks at the name of the field.
+
+    @PostConstruct
+    public void postConstruct() {
+        logger.info("postConstruct method called");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        logger.info("preDestroy method called");
+    }
 
     public int binarySearch(int[] haystack, int needle) {
         /*
