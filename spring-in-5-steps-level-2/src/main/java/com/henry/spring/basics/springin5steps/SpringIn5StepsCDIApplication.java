8fb683e6950b0f5ea150d7925037d8ed38acc98a
Henry Post <henryfbp@gmail.com>
2021-08-10 14:06:33 -0500

    migrate rest of classes off of spring boot starter

--- spring-in-5-steps-level-2/src/main/java/com/henry/spring/basics/springin5steps/SpringIn5StepsCDIApplication.java
+++ spring-in-5-steps-level-2/src/main/java/com/henry/spring/basics/springin5steps/SpringIn5StepsCDIApplication.java
@@ -1,26 +1,28 @@
-//package com.henry.spring.basics.springin5steps;
-//
-//import com.henry.spring.basics.springin5steps.cdi.SomeCDIBusiness;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.ApplicationContext;
-//
-//@SpringBootApplication
-//public class SpringIn5StepsCDIApplication {
-//
-//    private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsCDIApplication.class);
-//
-//    public static void main(String[] args) {
-//
-//        System.out.println("hello world!");
-//        ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsCDIApplication.class, args);
-//
-//        SomeCDIBusiness business = applicationContext.getBean(SomeCDIBusiness.class);
-//
-//        LOGGER.info("{} dao={}", business, business.getSomeCDIDAO());
-//
-//    }
-//
-//}
+package com.henry.spring.basics.springin5steps;
+
+import com.henry.spring.basics.springin5steps.cdi.SomeCDIBusiness;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.henry.spring.basics.springin5steps.cdi")
+public class SpringIn5StepsCDIApplication {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsCDIApplication.class);
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world!");
+        AnnotationConfigApplicationContext acac =
+                new AnnotationConfigApplicationContext(SpringIn5StepsCDIApplication.class);
+
+        SomeCDIBusiness business = acac.getBean(SomeCDIBusiness.class);
+
+        LOGGER.info("{} dao={}", business, business.getSomeCDIDAO());
+
+    }
+
+}
--- spring-in-5-steps-level-2/src/main/java/com/henry/spring/basics/springin5steps/SpringIn5StepsScopeApplication.java
+++ spring-in-5-steps-level-2/src/main/java/com/henry/spring/basics/springin5steps/SpringIn5StepsScopeApplication.java
@@ -1,32 +1,34 @@
-//package com.henry.spring.basics.springin5steps;
-//
-//import com.henry.spring.basics.springin5steps.scope.PersonDAO;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.ApplicationContext;
-//
-//@SpringBootApplication
-//public class SpringIn5StepsScopeApplication {
-//
-//    private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsScopeApplication.class);
-//
-//    public static void main(String[] args) {
-//
-//        System.out.println("hello world!");
-//        ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsScopeApplication.class, args);
-//
-//        PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
-//        PersonDAO personDAO2 = applicationContext.getBean(PersonDAO.class);
-//
-//        LOGGER.info("foobar123");
-//        LOGGER.info("{}", personDAO);
-//        LOGGER.info("{}", personDAO.getJdbcConnection());
-//        LOGGER.info("{}", personDAO.getJdbcConnection());
-//
-//        LOGGER.info("{}", personDAO2);
-//        LOGGER.info("{}", personDAO2.getJdbcConnection());
-//    }
-//
-//}
+package com.henry.spring.basics.springin5steps;
+
+import com.henry.spring.basics.springin5steps.scope.PersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.henry.spring.basics.springin5steps.scope")
+public class SpringIn5StepsScopeApplication {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsScopeApplication.class);
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world!");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringIn5StepsScopeApplication.class);
+
+        PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
+        PersonDAO personDAO2 = applicationContext.getBean(PersonDAO.class);
+
+        LOGGER.info("foobar123");
+        LOGGER.info("{}", personDAO);
+        LOGGER.info("{}", personDAO.getJdbcConnection());
+        LOGGER.info("{}", personDAO.getJdbcConnection());
+
+        LOGGER.info("{}", personDAO2);
+        LOGGER.info("{}", personDAO2.getJdbcConnection());
+    }
+
+}
