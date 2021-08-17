9efa97ccd3eae8063b61547d6a9502dfa30a9625
Henry Post <henryfbp@gmail.com>
2021-08-17 14:50:37 -0500

    add /books/ api

---
+++ springboot-in-10-steps/src/main/java/com/henry/springboot/basics/springbootin10steps/Book.kt
@@ -0,0 +1,3 @@
+package com.henry.springboot.basics.springbootin10steps
+
+class Book(var id: Long, var name: String, var author: String)
---
+++ springboot-in-10-steps/src/main/java/com/henry/springboot/basics/springbootin10steps/BookController.java
@@ -0,0 +1,20 @@
+package com.henry.springboot.basics.springbootin10steps;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    @GetMapping("/books")
+    public List<Book> getAllBooks(){
+        return Arrays.asList(
+                new Book(1,"Mastering Spring", "Ranga Karanam")
+        );
+        //gee, wonder how this returns JSON when queried...magic? or spring?
+    }
+
+}
