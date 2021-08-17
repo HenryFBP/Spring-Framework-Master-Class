62ac932e796f86e354381e2b49fed50d0139bfe6
Henry Post <henryfbp@gmail.com>
2021-08-16 20:09:42 -0500

    use xml file to scan packages for applicationContext

--- spring-in-5-steps-level-2/src/main/java/com/henry/spring/basics/springin5steps/SpringIn5StepsXMLContextApplication.java
+++ spring-in-5-steps-level-2/src/main/java/com/henry/spring/basics/springin5steps/SpringIn5StepsXMLContextApplication.java
@@ -3,12 +3,12 @@ package com.henry.spring.basics.springin5steps;
 import com.henry.spring.basics.springin5steps.xml.XMLPersonDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan("com.henry.spring.basics.springin5steps.basic")
+import java.util.Arrays;
+
+//@Configuration
+//@ComponentScan("com.henry.spring.basics.springin5steps.basic") //getting rid of these, moved to xml file...
 public class SpringIn5StepsXMLContextApplication {
 
     private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsScopeApplication.class);
@@ -20,6 +20,12 @@ public class SpringIn5StepsXMLContextApplication {
 
             LOGGER.info("Beans Loaded -> {}", (Object) applicationContext.getBeanDefinitionNames());
             // [xmlJdbcConnection, xmlPersonDAO]
+
+            LOGGER.info("Beans loaded from XML file:");
+            final int[] i = {0};
+            Arrays.stream(applicationContext.getBeanDefinitionNames()).forEach(
+                    x->LOGGER.info("{} {}", i[0]++,x)
+            );
 
             XMLPersonDAO personDao = applicationContext.getBean(XMLPersonDAO.class);
 
