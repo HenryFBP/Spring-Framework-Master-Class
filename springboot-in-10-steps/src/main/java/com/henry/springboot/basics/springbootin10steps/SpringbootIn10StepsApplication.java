7738603e669a77b95a98f44096f41474b1a89a99
Henry Post <henryfbp@gmail.com>
2021-08-17 16:02:46 -0500

    print spring boot auto config debug ingo

--- springboot-in-10-steps/src/main/java/com/henry/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ springboot-in-10-steps/src/main/java/com/henry/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -2,12 +2,17 @@ package com.henry.springboot.basics.springbootin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringbootIn10StepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+		ApplicationContext ac = SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+
+		Arrays.stream(ac.getBeanDefinitionNames()).forEachOrdered(System.out::println);
 	}
 
 }
