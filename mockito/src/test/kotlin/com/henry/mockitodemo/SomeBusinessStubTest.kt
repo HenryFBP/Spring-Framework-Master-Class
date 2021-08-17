c8aa5119c2bd0ee681ed31592f3e8ff505cd31cf
Henry Post <henryfbp@gmail.com>
2021-08-17 12:25:01 -0500

    add 2nd mock example

--- mockito/src/test/kotlin/com/henry/mockitodemo/SomeBusinessStubTest.kt
+++ mockito/src/test/kotlin/com/henry/mockitodemo/SomeBusinessStubTest.kt
@@ -1,12 +1,11 @@
 package com.henry.mockitodemo
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SomeBusinessTest {
+class SomeBusinessStubTest {
 
     @Test
     fun testFindGreatest() {
