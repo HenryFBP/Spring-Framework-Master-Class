2387a7ec2ef13138f99a7ce9fa47e6a5e1831a64
Henry Post <henryfbp@gmail.com>
2021-08-17 13:51:11 -0500

    add xml test context

---
+++ spring-in-5-steps/src/test/java/com/henry/spring/basics/springin5steps/BinarySearchXMLConfigTest.java
@@ -0,0 +1,27 @@
+package com.henry.spring.basics.springin5steps;
+
+import com.henry.spring.basics.springin5steps.basic.BinarySearchImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = "/testContext.xml") //xml locations
+public class BinarySearchXMLConfigTest {
+
+	@Autowired
+	BinarySearchImpl binarySearch;
+
+	@Test
+	public void testBinarySearch() {
+		int result = binarySearch.binarySearch(new int[]{1, 2, 3, 4},3);
+		System.out.println(result);
+
+		assertEquals(3, result);
+	}
+
+}
